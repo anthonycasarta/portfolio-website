d13518e948e0101e35c2a5e3cab11e25bf0e5b41
anthonycasarta <anthonycasarta@gmail.com>
2025-08-03 14:19:08 -0500

    add express app

---
+++ server/src/expressApp.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+export const expressApp = () => {
+  const app = express();
+
+  app.use(cors);
+
+  app.get("/api", (req, res) => {
+    res.json({ message: "Server Running" });
+  });
+};
