8626510fce3734e6a7e47a52123dc42d691441fd
anthonycasarta <anthonycasarta@gmail.com>
2025-08-03 14:29:29 -0500

    implement index.ts

--- server/src/expressApp.ts
+++ server/src/expressApp.ts
@@ -4,9 +4,12 @@ import cors from "cors";
 export const expressApp = () => {
   const app = express();
 
-  app.use(cors);
+  app.use(cors());
+  app.use(express.json());
 
   app.get("/api", (req, res) => {
     res.json({ message: "Server Running" });
   });
+
+  return app;
 };
