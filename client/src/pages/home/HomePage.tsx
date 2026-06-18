7078f1b9514aabef69ac1a17894857e94456e38a
anthonycasarta <anthonycasarta@gmail.com>
2026-06-18 17:00:48 -0500

    add recent projects section

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,9 +1,13 @@
+import { RecentProjects } from "./home-features";
 import "./HomePage.css";
 
 export const HomePage = () => {
   return (
     <div className="home-page">
       <h1>Home Page</h1>
+      <div>
+        <RecentProjects />
+      </div>
     </div>
   );
 };
---
+++ client/src/pages/home/home-features/recent-projects/RecentProjects.tsx
@@ -0,0 +1,13 @@
+import "./RecentProjects.css";
+
+export const RecentProjects = () => {
+  return (
+    <div className="recent-projects__section">
+      <h1 className="title">Recent Projects</h1>
+      <div className="project-list">
+        <div className="recent-projects__card">Example Project</div>
+        <div className="recent-projects__card">Example Project 2</div>
+      </div>
+    </div>
+  );
+};
